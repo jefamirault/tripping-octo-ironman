087567a3a4797959c659f76d4befce8651f03e2e
GagnonStyle <spencer.gagnon95@gmail.com>
2015-04-11 23:30:13 -0400

    hardcoded UI now shows up

--- skela/app/assets/javascripts/commandr/commandr.js
+++ skela/app/assets/javascripts/commandr/commandr.js
@@ -21,6 +21,68 @@ window.Commandr = (function(){
   return commandr;
 }());
 
+
+
+$(function(){
+  var container = $('<div></div>').addClass('commander');
+  var icon = $('<div></div>').addClass('commander-icon');
+  var textContainer = $('<div></div>').addClass('commander-text-container');
+  var pic = $('<img />').attr("src", "http://png-4.findicons.com/files/icons/2579/iphone_icons/40/radio_microphone.png");
+  var banner = $('<div></div>').addClass('commander-banner');
+  var spoken = $('<div></div>').addClass('commander-spoken');
+  icon.append(pic);
+  container.append(icon);
+  textContainer.append(banner);
+  textContainer.append(spoken);
+  container.append(textContainer);
+
+  container.css({
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: '#ddd',
+    width: '300px',
+    height: '75px',
+    border: '2px solid #999',
+    borderTop: 'none',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    borderBottomRightRadius: '20px',
+    borderBottomLeftRadius: '20px',
+    boxShadow: '2px 2px 1px #888'
+  });
+
+  icon.css({
+    display: 'inline-block',
+    marginLeft: '20px',
+    marginTop: '2px'
+  });
+
+  banner.css({
+    verticalAlign: 'top'
+  });
+
+  spoken.css({
+    width: '200px',
+    height: '10px',
+    display: 'inline-block'
+  });
+
+  textContainer.css({
+    display: 'inline-block',
+    width: '200px',
+    marginLeft: '30px'
+  });
+
+  $('body').prepend(container);
+
+  banner.text("Commander...");
+  spoken.text("bananas");
+});
+
+
+
 console.log("starting registrations");
 Commandr.register("hello",function(){console.log("register worked (hello)");});
 Commandr.register("goodbye",function(){console.log("register worked (goodbye)");});
