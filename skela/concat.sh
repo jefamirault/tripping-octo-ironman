2bf8c15e81f7661001d9c55cffd4030aa8d57392
Christopher Ciollaro <cciollaro@gmail.com>
2015-04-12 14:41:16 -0400

    updated concat to move commandr.js to commandr.me

--- skela/concat.sh
+++ skela/concat.sh
@@ -1,2 +1,3 @@
 cd app/assets/javascripts/commandr
 cat browser-detect.js socket.io.js speech-recognizer.js jquery-1.11.1.min.js commandr.js s-to-t-demo.js t-to-s-demo.js > concat.js
+mv app/assets/javascripts/commandr/concat.js ~/commandr.me/commandr.js
