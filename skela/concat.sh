b54cb5e4315c3d018f69049944ae976622f0a06e
Christopher Ciollaro <cciollaro@gmail.com>
2015-04-12 14:49:40 -0400

    updated concat

--- skela/concat.sh
+++ skela/concat.sh
@@ -1,3 +1,2 @@
 cd app/assets/javascripts/commandr
-cat browser-detect.js socket.io.js speech-recognizer.js jquery-1.11.1.min.js commandr.js s-to-t-demo.js t-to-s-demo.js > concat.js
-mv app/assets/javascripts/commandr/concat.js ~/commandr.me/commandr.js
+cat browser-detect.js socket.io.js speech-recognizer.js jquery-1.11.1.min.js commandr.js s-to-t-demo.js t-to-s-demo.js > $HOME/commandr.me/commandr.js
